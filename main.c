6b05b99f13752869f38deae71a1859b822b97122
dang0x5f <dannypgehrz@gmail.com>
2024-08-20 19:06:52 -0500

    Added an array of pointers to hold all the words randomized.

--- main.c
+++ main.c
@@ -6,7 +6,8 @@
 
 //"/usr/share/misc/iso3166",
 
-const int filecount = 4;
+const int loops         = 5;
+const int filecount     = 4;
 const char* filepaths[] = {
     "/usr/share/dict/web2",
     "/usr/share/dict/web2a",
@@ -16,20 +17,30 @@ const char* filepaths[] = {
 
 void openfiles(FILE**);
 void assessfiles(FILE**,int*);
-void randomwords(FILE**,int*,char*,int);
+void randomwords(FILE**,int*,char**,int);
+void freebunch(char**,int);
 void closefiles(FILE**);
 
 int main(void){
     FILE* files[filecount];    
     int file_lengths[filecount];
-    char bunch_o_words[1024];
+    char* wordset[loops*filecount];
 
     openfiles(files);
 
-    assessfiles(files, file_lengths);
+    assessfiles(files,file_lengths);
 
-    randomwords(files, file_lengths, bunch_o_words, 1024);
+    randomwords(files,file_lengths,wordset,loops);
 
+    int totalbytes = 0;
+    for(int x = 0; x < loops*filecount; x++){
+        printf("%s,", wordset[x]);
+        totalbytes += strlen(wordset[x]);
+    }
+    printf("\n%d\n", totalbytes);
+    printf("\n%lu\n", sizeof(wordset));
+
+    freebunch(wordset,loops*filecount);
     closefiles(files);
     return(0);
 }
@@ -55,26 +66,38 @@ void assessfiles(FILE** files, int* file_lengths){
     }
 }
 
-void randomwords(FILE** files, int* file_lengths, char* buffer, int bufsize){
-    int randomline, offset;
+void randomwords(FILE** files, int* file_lengths, char** words, int loops){
+    char buffer[256];
+    int randomline, offset, index = 0;
 
     srand(time(NULL));
-    for(int j = 0; j < 5; j++){
+    for(int j = 0; j < loops; j++){
         for(int i = 0; i < filecount; i++){
 
             offset = 0;
             randomline = ( (int)(( (double)rand()/RAND_MAX)*file_lengths[i]) );
 
             for(int x = 1; x < randomline; x++)
-                offset += strlen(fgets(buffer, bufsize, files[i]));
+                offset += strlen(fgets(buffer,256,files[i]));
+
 
             fseek(files[i], offset, SEEK_SET);
-            printf("%s", fgets(buffer,bufsize,files[i]));
-            /* fgets(buffer,bufsize,files[i]); */
-            /* buffer[strlen(buffer) - 1] = ' '; */
+            fgets(buffer,256,files[i]);
+            buffer[strlen(buffer) - 1] = '\0';
+            words[index] = calloc(strlen(buffer)+1,sizeof(char));
+            strcpy(words[index],buffer);
+
             /* printf("%s", buffer); */
+
             rewind(files[i]);
+            index++;
         }
+    }
+}
+
+void freebunch(char** words, int len){
+    for(int x = 0; x < len; x++){
+        free(words[x]);
     }
 }
 
