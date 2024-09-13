6243fba7bdd79a9920e5a5eb64d1d771774f20ce
dang0x5f <dannypgehrz@gmail.com>
2024-09-13 18:08:41 -0500

    semi-working prototype with u4test.c, TODO at top explains an annoying occurrence between the buffer comparison and text segment, but also a possible solution to try and implement.

--- pieces/u2test.c
+++ pieces/u2test.c
@@ -8,20 +8,49 @@ int seg_start, seg_end;
 enum { CURSOR, SAVEON, SAVEW8, NEUTRAL, CORRECT, INCORRECT };
 
 #define KEY_SPACE 32
-int fail_state = 0;                                 // 
+int fail_state =  0;                                 // 
+int fail_index = -1;
 
+int clear_prompt_buffer(char [], int);
+int set_new_segments(char [], int, int);
 int check_segments(char []);                        //
 int init_seg(char [], int);
 int write_text(char* , char [], int);
 int init_text_state(char* , char []);
-int set_text_state(char* , char [], int , int);
+int set_text_state(char* , char [], int , int, char [], int*);
 int write_char(int , char [], int);
 
+int clear_prompt_buffer(char buffer[], int len){
+    for(int x = 0; x < len; x++)
+        buffer[x] = '\0';
+    flushinp();
+    
+    wmove(prompt,1,1);
+    for(int x = 1; x <= len; x++){
+        waddch(prompt,' ');
+    }
+
+    wmove(prompt,1,1);
+    wrefresh(stdscr);
+    wrefresh(prompt);
+
+    return(0);
+}
+
+int set_new_segments(char state[], int start, int len){
+    int x;
+    seg_start = start;
+    for(x = start; x < len && state[x] != SAVEW8; x++);
+    seg_end = x;
+
+    return(0);
+}
+
 int check_segments(char state[]){                   //
-    int failed = 0;                                 //
+    int failed = -1;                                 //
     for(int x = seg_start; x < seg_end; x++){       //
         if(state[x] == INCORRECT)                   //
-            failed = 1;                             //
+            failed = x;                             //
     }                                               //
     return(failed);                                 //
 }                                                   //
@@ -44,7 +73,10 @@ int write_text(char* text, char state[], int text_index){
         if( state[x] == CURSOR ){
             addch(A_REVERSE | *(text+x));
         }
-        else if( state[x] == SAVEW8 || state[x] == SAVEON){
+        else if(state[x] == SAVEON ){
+            addch(COLOR_PAIR(3) | *(text+x));
+        }
+        else if( state[x] == SAVEW8 ){
             addch(*(text+x));
         }
         else if( state[x] == NEUTRAL ){
@@ -85,7 +117,7 @@ int init_text_state(char* text, char state[]){
     return(0);
 }
 
-int set_text_state(char* text, char state[], int c, int index){
+int set_text_state(char* text, char state[], int c, int index, char buffer[], int* buffer_index){
 
     switch(c){
         case KEY_DC:
@@ -101,10 +133,19 @@ int set_text_state(char* text, char state[], int c, int index){
                 state[index] = INCORRECT;
             }
             else{
+                if(check_segments(state) >= 0){
+                    state[index] = INCORRECT;
+                    fail_state = 1;
+                    fail_index = index;
+                }
+                else{
+                    /* state[index] = CORRECT; */
+                    state[index] = SAVEON;
+                    set_new_segments(state,index+1,strlen(text));
+                    *buffer_index = clear_prompt_buffer(buffer,48);
+                }
                 // check
                 // if failed
-                //    set fail state TODO: alt set_text_state while in fail state
-                //    INCORRECT
                 // else
                 //    if game not over
                 //        set new segs
@@ -129,10 +170,6 @@ int set_text_state(char* text, char state[], int c, int index){
             break;
         default:
             state[index] = (*(text+index) == c) ? CORRECT : INCORRECT;
-            /* if(*(text+index) == c) */
-            /*     state[index] = CORRECT; */
-            /* else */
-            /*     state[index] = INCORRECT; */
             break;
     }
 
@@ -143,36 +180,51 @@ int set_text_state(char* text, char state[], int c, int index){
     else{
         return(index);
     }
+}
 
-/*
-    if(c == KEY_BACKSPACE){
-        if(index > 0){
-            state[index] = NEUTRAL;
-            state[index - 1] = CURSOR;
-            return(index - 1);
-        }
-        else{
+int set_fail_state(char* text, char state[], int c, int index){
+
+    switch(c){
+        case KEY_DC:
+        case KEY_IC:
+        case KEY_HOME:
+        case KEY_END:
+        case KEY_NPAGE:
+        case KEY_PPAGE:
             return(index);
-        }
+            break;
+        case KEY_BACKSPACE:
+            if(fail_index == index)
+                fail_state = 0;
+            if(index > 0){
+                state[index] = (*(text+index) == ' ') ? SAVEW8 : NEUTRAL;
+                state[index - 1] = CURSOR;
+                return(index - 1);
+            }
+            else{
+                return(index);
+            }
+            break;
+        default:
+            state[index] = INCORRECT;
+            break;
     }
 
-    if(*(text+index) == c)
-        state[index] = CORRECT;
-    else
-        state[index] = INCORRECT;
-
-    if(index < strlen(text)-1){
-        state[index + 1] = CURSOR;
-        return(index + 1);
-    }    
+    if(index < strlen(text) - 1){
+        state[index+1] = CURSOR;
+        return(index+1);
+    }
     else{
         return(index);
     }
-*/
 }
 
 int write_char(int c, char buffer[], int buffer_index){
     int r = buffer_index;
+
+    if(c == KEY_SPACE && r == 0)
+        return(buffer_index);
+    
     if(c == KEY_DC || c == KEY_IC || c == KEY_NPAGE || c == KEY_PPAGE || c == KEY_HOME || c == KEY_END)
         return(buffer_index);
 
@@ -208,7 +260,7 @@ int main(void){
     char buffer[pcols - 2];
     int  buffer_index = 0;
 
-    char* text = "one two three";
+    char* text = "foobar foo bar f b!";
     char  text_state[strlen(text)];
     int   text_index = 0;
 
@@ -219,6 +271,9 @@ int main(void){
     start_color();
     init_pair(1, COLOR_BLACK, COLOR_GREEN);
     init_pair(2, COLOR_BLACK, COLOR_RED  );
+    /* init_pair(3, COLOR_BLACK, COLOR_CYAN ); */
+    init_color(33,0,0,1000);
+    init_pair(3, COLOR_BLACK, 33);
     refresh();
 
     prompt = newwin(prows, pcols, 5, 10);
@@ -233,7 +288,10 @@ int main(void){
     while(true){
         write_text(text,text_state,text_index);
         c = wgetch(prompt);
-        text_index = set_text_state(text, text_state, c, text_index);
+        if(!fail_state)
+            text_index = set_text_state(text, text_state, c, text_index, buffer, &buffer_index);
+        else
+            text_index = set_fail_state(text, text_state, c, text_index);
         buffer_index = write_char(c, buffer, buffer_index);
 
         /* printw("%d",text_index); */
--- pieces/u4test.c
+++ pieces/u4test.c
@@ -1,3 +1,10 @@
+/* TODO: backspacing should require erasing everything back   */
+/*       to buffer/prompt beginning.                          */ 
+/*       this might require reworking the logic of checking   */
+/*       segments, for example temp string of buffer could be */
+/*       compared with temp string of text. Both extracted as */
+/*       dynamically allocated strings.                       */
+
 #include <ncurses.h>
 #include <string.h>
 
@@ -8,23 +15,52 @@ int seg_start, seg_end;
 enum { CURSOR, SAVEON, SAVEW8, NEUTRAL, CORRECT, INCORRECT };
 
 #define KEY_SPACE 32
-int fail_state = 0;                                 // 
+int fail_state =  0;
+int fail_index = -1;
 
-int check_segments(char []);                        //
+int clear_prompt_buffer(char [], int);
+int set_new_segments(char [], int, int);
+int check_segments(char []);
 int init_seg(char [], int);
 int write_text(char* , char [], int);
 int init_text_state(char* , char []);
-int set_text_state(char* , char [], int , int);
+int set_text_state(char* , char [], int , int, char [], int*);
 int write_char(int , char [], int);
 
-int check_segments(char state[]){                   //
-    int failed = 0;                                 //
-    for(int x = seg_start; x < seg_end; x++){       //
-        if(state[x] == INCORRECT)                   //
-            failed = 1;                             //
-    }                                               //
-    return(failed);                                 //
-}                                                   //
+int clear_prompt_buffer(char buffer[], int len){
+    for(int x = 0; x < len; x++)
+        buffer[x] = '\0';
+    flushinp();
+    
+    wmove(prompt,1,1);
+    for(int x = 1; x <= len; x++){
+        waddch(prompt,' ');
+    }
+
+    wmove(prompt,1,1);
+    wrefresh(prompt);
+
+    return(0);
+}
+
+int set_new_segments(char state[], int start, int len){
+    int x;
+    seg_start = start;
+    for(x = start; x < len && state[x] != SAVEW8; x++);
+    seg_end = x;
+    /* printw("start: %d end: %d", seg_start, seg_end); */
+
+    return(0);
+}
+
+int check_segments(char state[]){
+    int failed = -1;
+    for(int x = seg_start; x < seg_end; x++){
+        if(state[x] == INCORRECT)
+            failed = x;
+    }
+    return(failed);
+}
 
 int init_seg(char state[], int size){
     int x;
@@ -44,7 +80,10 @@ int write_text(char* text, char state[], int text_index){
         if( state[x] == CURSOR ){
             addch(A_REVERSE | *(text+x));
         }
-        else if( state[x] == SAVEW8 || state[x] == SAVEON){
+        else if(state[x] == SAVEON ){
+            addch(COLOR_PAIR(3) | *(text+x));
+        }
+        else if( state[x] == SAVEW8 ){
             addch(*(text+x));
         }
         else if( state[x] == NEUTRAL ){
@@ -85,8 +124,7 @@ int init_text_state(char* text, char state[]){
     return(0);
 }
 
-int set_text_state(char* text, char state[], int c, int index){
-
+int set_text_state(char* text, char state[], int c, int index, char buffer[], int* buffer_index){
     switch(c){
         case KEY_DC:
         case KEY_IC:
@@ -97,28 +135,34 @@ int set_text_state(char* text, char state[], int c, int index){
             return(index);
             break;
         case KEY_SPACE:
-            if(*(text+index) != KEY_SPACE){
+            // char* text should be null terminated
+            if(*(text+index) != KEY_SPACE && index == strlen(text)-1){     
+                if(check_segments(state) >= 0){
+                    return(index);
+                }
+                else{
+                    printw("%s", "W");
+                    *buffer_index = clear_prompt_buffer(buffer,48);
+                }
+            }
+            else if(*(text+index) != KEY_SPACE && index < strlen(text)-1){
                 state[index] = INCORRECT;
             }
             else{
-                // check
-                // if failed
-                //    set fail state TODO: alt set_text_state while in fail state
-                //    INCORRECT
-                // else
-                //    if game not over
-                //        set new segs
-                //        clear buffer
-                //        flushinp (clean start)
-                //        LOCK all up to new seg_beg
-                //        set new offset so backspace logic doesnt erase work(global offset = newsegbeg)
-                //    else
-                //        GG, exit with stats or new game w/e
-                // 
+                if(check_segments(state) >= 0){
+                    state[index] = INCORRECT;
+                    fail_state = 1;
+                    fail_index = index;
+                }
+                else{
+                    state[index] = SAVEON;
+                    set_new_segments(state,index+1,strlen(text));
+                    *buffer_index = clear_prompt_buffer(buffer,48);
+                }
             }
             break;
         case KEY_BACKSPACE:
-            if(index > 0){
+            if(index > seg_start){
                 state[index] = NEUTRAL;
                 state[index - 1] = CURSOR;
                 return(index - 1);
@@ -129,10 +173,6 @@ int set_text_state(char* text, char state[], int c, int index){
             break;
         default:
             state[index] = (*(text+index) == c) ? CORRECT : INCORRECT;
-            /* if(*(text+index) == c) */
-            /*     state[index] = CORRECT; */
-            /* else */
-            /*     state[index] = INCORRECT; */
             break;
     }
 
@@ -143,36 +183,51 @@ int set_text_state(char* text, char state[], int c, int index){
     else{
         return(index);
     }
+}
 
-/*
-    if(c == KEY_BACKSPACE){
-        if(index > 0){
-            state[index] = NEUTRAL;
-            state[index - 1] = CURSOR;
-            return(index - 1);
-        }
-        else{
+int set_fail_state(char* text, char state[], int c, int index){
+
+    switch(c){
+        case KEY_DC:
+        case KEY_IC:
+        case KEY_HOME:
+        case KEY_END:
+        case KEY_NPAGE:
+        case KEY_PPAGE:
             return(index);
-        }
+            break;
+        case KEY_BACKSPACE:
+            if(fail_index == index)
+                fail_state = 0;
+            if(index > 0){
+                state[index] = (*(text+index) == ' ') ? SAVEW8 : NEUTRAL;
+                state[index - 1] = CURSOR;
+                return(index - 1);
+            }
+            else{
+                return(index);
+            }
+            break;
+        default:
+            state[index] = INCORRECT;
+            break;
     }
 
-    if(*(text+index) == c)
-        state[index] = CORRECT;
-    else
-        state[index] = INCORRECT;
-
-    if(index < strlen(text)-1){
-        state[index + 1] = CURSOR;
-        return(index + 1);
-    }    
+    if(index < strlen(text) - 1){
+        state[index+1] = CURSOR;
+        return(index+1);
+    }
     else{
         return(index);
     }
-*/
 }
 
 int write_char(int c, char buffer[], int buffer_index){
     int r = buffer_index;
+
+    if(c == KEY_SPACE && r == 0)
+        return(buffer_index);
+    
     if(c == KEY_DC || c == KEY_IC || c == KEY_NPAGE || c == KEY_PPAGE || c == KEY_HOME || c == KEY_END)
         return(buffer_index);
 
@@ -208,7 +263,7 @@ int main(void){
     char buffer[pcols - 2];
     int  buffer_index = 0;
 
-    char* text = "one two three";
+    char* text = "foobar foo bar f b!";
     char  text_state[strlen(text)];
     int   text_index = 0;
 
@@ -219,6 +274,9 @@ int main(void){
     start_color();
     init_pair(1, COLOR_BLACK, COLOR_GREEN);
     init_pair(2, COLOR_BLACK, COLOR_RED  );
+    /* init_pair(3, COLOR_BLACK, COLOR_CYAN ); */
+    init_color(33,0,0,1000);
+    init_pair(3, COLOR_BLACK, 33);
     refresh();
 
     prompt = newwin(prows, pcols, 5, 10);
@@ -233,7 +291,10 @@ int main(void){
     while(true){
         write_text(text,text_state,text_index);
         c = wgetch(prompt);
-        text_index = set_text_state(text, text_state, c, text_index);
+        if(!fail_state)
+            text_index = set_text_state(text, text_state, c, text_index, buffer, &buffer_index);
+        else
+            text_index = set_fail_state(text, text_state, c, text_index);
         buffer_index = write_char(c, buffer, buffer_index);
 
         /* printw("%d",text_index); */
